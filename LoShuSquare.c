fe2eef830067a94ded04c4533f01c473909addfc
Farid Vakili <fvakili@cpp.edu>
2022-10-21 18:53:22 -0700

    Initial Commit: OUTLINE of LoShuSquare.c program

---
+++ LoShuSquare.c
@@ -0,0 +1,36 @@
+// git remote add origin https://github.com/justFreds/Lo_Shu_Magic_Square.git
+// git branch -M main
+// git push -u origin main
+
+//LO SHU MAGIC SQUARE
+
+
+//MAIN
+    //CREATE TWO 2D ARRAYS
+        //1-LO SHU SQUARE
+        //2-NOT
+
+    //TEMP NUMBER ARRAY FROM 1-9 (temp swap for randomizing)?
+
+    //PASS ARRAYS INTO isLoShu([][]);
+
+    
+    //createSquare()
+    //printSquare()
+    //isLoShu()
+
+
+//FUNCTIONS
+    //1)
+        //BOOL FUNCTION isLoShu([][]) THAT ACCEPTS 2D ARRAY AS ARG RETURNS IF MAGIC SQUARE OR NOT
+
+    //2)
+        //FUNCITON createSquare()
+        
+        //FUNCITON printSquare([][])
+            //Print two things:
+            // a.	The total number of squares generated and tested before success, the count.
+            // b.	Print the square and show the placement of values in the following format:
+            // [a b c]
+            // [d e f]
+            // [g h i]
